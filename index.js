a72f314f9a14ca2f0b31308a6b0170135ea249f1
khushi <110249789+KHUSHI2021@users.noreply.github.com>
2023-07-08 20:12:41 +0530

    Add files via upload
    This is a Netflix-responsive website with JAVASCRIPT,HTML,CSS

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
-// This function clear all the values
-function clearScreen() {
-    document.getElementById("result").value = "";
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+
+// Select tab content item
+function selectItem(e) {
+	// Remove all show and border classes
+	removeBorder();
+	removeShow();
+	// Add border to current tab item
+	this.classList.add('tab-border');
+	// Grab content item from DOM
+	const tabContentItem = document.querySelector(`#${this.id}-content`);
+	// Add show class
+	tabContentItem.classList.add('show');
 }
- 
-// This function display values
-function display(value) {
-    document.getElementById("result").value += value;
+
+// Remove bottom borders from all tab items
+function removeBorder() {
+	tabItems.forEach(item => {
+		item.classList.remove('tab-border');
+	});
 }
- 
-// This function evaluates the expression and returns result
-function calculate() {
-    var p = document.getElementById("result").value;
-    var q = eval(p);
-    document.getElementById("result").value = q;
+
+// Remove show class from all content items
+function removeShow() {
+	tabContentItems.forEach(item => {
+		item.classList.remove('show');
+	});
 }
+
+// Listen for tab item click
+tabItems.forEach(item => {
+	item.addEventListener('click', selectItem);
+});
+//https://cellularnews.com/wp-content/uploads/2020/05/Funimation-anime-600x376.jpeg
+
+
+//https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzk5I8_XNSMCEXAliq3HYvAI6g8v4ZUKzvwg&usqp=CAU
